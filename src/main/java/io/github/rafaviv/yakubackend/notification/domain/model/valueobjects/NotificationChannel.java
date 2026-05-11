f096ee4bd87b47b8a8f608b84fd76ce0029ea8ae
RafaelVivanco <rafaelvivanco2@gmail.com>
2026-05-11 16:07:47 -0500

    chore(notification): refactor notification logic

--- src/main/java/io/github/rafaviv/yakubackend/notification/domain/model/valueobjects/NotificationChannel.java
+++
@@ -1,7 +0,0 @@
-package io.github.rafaviv.yakubackend.notification.domain.model.valueobjects;
-
-public enum NotificationChannel {
-    EMAIL,
-    SMS,
-    PUSH
-}
--- src/main/java/io/github/rafaviv/yakubackend/notification/infrastructure/persistence/jpa/repositories/AlertRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.rafaviv.yakubackend.notification.infrastructure.persistence.jpa.repositories;
-
-import io.github.rafaviv.yakubackend.notification.domain.model.aggregates.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-}
